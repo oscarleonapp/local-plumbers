3c65bb1e5baa07da3e9028420f74483246e69ab0
oscarleon <oscarleon.alcatel@gmai.com>
2023-09-29 17:33:53 -0600

    reviews mostrar.ocultar

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,24 +1,26 @@
 /*==================== SHOW MENU ====================*/
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-    
-    // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
-            nav.classList.toggle('show-menu')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  // Validate that variables exist
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      // We add the show-menu class to the div tag with the nav__menu class
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav_link')
+const navLink = document.querySelectorAll(".nav_link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== SHOW REVIEWS ====================*/
